baebee8548e11341aa7f56f3511d6547684fc26a
malik <malikabdallah75019@gmail.com>
2024-11-30 16:36:35 +0200

    reactive statement

--- svelte-course/src/lib/Counter.svelte
+++ svelte-course/src/lib/Counter.svelte
@@ -4,12 +4,19 @@
     function addCount(){
       count++;
     }
+    //let chaineDeCharactere = "";
+    $:chaineDeCharactere = `Vous avez cliquez ${count} fois.`
+ 
+    $: if(count >5 ){
+      console.log(`vous avez cliquez ${count} fois`)
+    }
 </script>
 
 <button on:click={(e) => {count++}} > Clicks {count}</button>
 <button on:click={addCount} > Clicks {count}</button>
 
-
+<br/>
+<h1>{chaineDeCharactere}</h1>
 <style>
   button{
     padding:15px;
